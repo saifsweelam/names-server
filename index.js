2862967866b8fd219c557cba7aeb2374bff359aa
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-10-05 01:53:42 +0300

    feat: Add initial code for HTTP server and names file

---
+++ index.js
@@ -0,0 +1,25 @@
+import http from 'node:http';
+import fs from 'node:fs';
+
+async function getNames() {
+    return await fs.promises.readFile('names.txt', 'utf8');
+}
+
+async function addName(name) {
+    const names = await getNames();
+    await fs.promises.writeFile('names.txt', names + '\n' + name);
+}
+
+const server = http.createServer(async (req, res) => {
+    if (req.url === '/names') {
+        const names = await getNames();
+        res.end(names);
+    } else if (req.url === '/add') {
+        await addName('Alice');
+        res.end('Name added');
+    } else {
+        res.end('Not found');
+    }
+});
+
+server.listen(3000);
