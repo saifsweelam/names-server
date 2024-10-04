b8c0d9e467027c8600fef4def0cb651233279a0a
Seif El-Din Sweilam <saifsweelam@gmail.com>
2024-10-05 01:54:41 +0300

    chore: Create an intentional typo

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import http from 'node:http';
 import fs from 'node:fs';
 
-async function getNames() {
+async function getNamas() {
     return await fs.promises.readFile('names.txt', 'utf8');
 }
 
 async function addName(name) {
-    const names = await getNames();
+    const names = await getNamas();
     await fs.promises.writeFile('names.txt', names + '\n' + name);
 }
 
 const server = http.createServer(async (req, res) => {
     if (req.url === '/names') {
-        const names = await getNames();
+        const names = await getNamas();
         res.end(names);
     } else if (req.url === '/add') {
         await addName('Alice');
