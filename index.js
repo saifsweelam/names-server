3a90d1def1db644cab2446080071faa0a0e4747d
ahmed elgaml <a7medelgaml11a@gmail.com>
2024-10-08 07:41:08 -0700

    add function to delete name from names file

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import http from 'node:http';
 import fs from 'node:fs';
 import{getNames,addName} from './index2.js'
 
-
+async function deletename (name) {
+    let allnames = await getNames();
+    let modifiednames= allnames.split('\n').filter((eachname)=>{
+        eachname!==name;
+    }).join('\n');
+    await fs.promises.writeFile('names.txt',modifiednames)
+}
 const server = http.createServer(async (req, res) => {
     if (req.url === '/names') {
         const names = await getNamas();
