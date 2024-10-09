844740c6a82c99e44cae319755e6fd1f82e2f96e
Elshahaby <motionalname@gmail.com>
2024-10-09 07:05:14 +0300

    feat: adding the deleting name feature
    - add a deletename function in server-functions.js and export it.
    - In index.js: I import deletename function
    - If I want to delete a name, I handle the case of not passing a name
    
    BREAKING CHANGE: We add a feature that delete a specific name and handle cases of not finding the
    name or not pass the name

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import http from 'node:http';
 import url from 'node:url';
-import { getNames,addName } from './server-functions.js';
+import { getNames,addName,deletename } from './server-functions.js';
 
 
 const server = http.createServer(async (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname.slice(1);  
     const query = parsedUrl.query;
+    const request = req;
+    const response = res;
 
     if (pathname === 'names') {
         const names = await getNames();
@@ -18,7 +20,16 @@ const server = http.createServer(async (req, res) => {
         const name = JSON.parse(query.name);
         await addName(name);
         res.end('Name added');
-    }else {
+    }
+    else if(pathname === 'delete'){
+        if(!query.name) {
+            res.end(`Please provide a name to delete.`);
+        }
+        const name = JSON.parse(query.name);
+        await deletename(name,request,response);
+        res.end('Name deleted');
+    }
+    else {
         res.end('404 Not Found');
     }
 });
